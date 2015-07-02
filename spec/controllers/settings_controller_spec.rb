bd1497b68e3276cfd062e89fadc9712cebe351cc
Derek Covey <derek.covey@gmail.com>
2015-07-01 21:44:19 -0700

    HW01 finished, but having issue with functional re-direct test

--- spec/controllers/settings_controller_spec.rb
+++ spec/controllers/settings_controller_spec.rb
@@ -2,10 +2,58 @@ require 'spec_helper'
 
 describe SettingsController, type: :controller do
 
-  describe "#index" do
+  let(:user) { create(:user) }
+  let(:attributes) { attributes_for :user }
+
+  before do
+    allow(self.controller).to receive(:current_user).and_return(user)
   end
 
-  describe "#update" do
+  describe "GET #index" do
+    it "assigns current_user to @settings" do
+      get :index
+      expect(assigns(:settings)).to eq(user)
+    end
+
+    it "is successful" do
+      get :index
+      expect(response).to be_success
+    end
+
+    it "renders the index view" do
+      get :index
+      expect(response).to render_template("index")
+    end
+  end
+
+  describe "POST #update" do
+    context "with valid attributes" do
+      it "updates attributes of @settings" do
+        post :update, :settings => {name: "New Name", email: "new@email.com"}
+        expect(user[:name]).to eq("New Name")
+        expect(user[:email]).to eq("new@email.com")
+      end
+
+      it "redirects to the settings page" do
+        subject { post :update, :settings => {name: "New Name", email: "new@email.com"} }
+        expect(subject).to redirect_to(settings_url)
+      end
+    end
+
+    context "with invalid attributes" do
+      it "does not update attributes of @settings" do
+        name_test = user[:name]
+        email_test = user[:email]
+        post :update, :settings => {name: "New Name", email: "bad_email"}
+        expect(user[:name]).to eq(name_test)
+        expect(user[:email]).to eq(email_test)
+      end
+
+      it "redirects to the setting page" do
+        subject { post :update, :settings => {name: "New Name", email: "bad_email"} }
+        expect(subject).to redirect_to(settings_url)
+      end
+    end
   end
 
 
