ebc3264e44efd378f4ae75e06e30c68c232e4b54
Derek Covey <derek.covey@gmail.com>
2015-07-15 19:06:26 -0700

    fixed error in LinksController.  Was making repeat short_urls for a single long_url

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -7,17 +7,25 @@ class LinksController < ApplicationController
   def create
     url = Link.find_by_long_url(params[:link][:long_url])
 
-    if url.blank?
-      if signed_in?  #i.e. url DID NOT already exist in :link model
+    if url.blank? #i.e. url DID NOT already exist in :link model
+      if signed_in?
         @link = current_user.links.build(link_params)
       else
         @link = Link.new(link_params)
       end
     else  #i.e. url DID already exist in :link model
       if signed_in?
-        flash[:error] = "This link already exists"
-        redirect_to root_path and return
-        #@link = current_user.links.build(link_params)
+        #changed the following so that a new link isn't made each
+        #time the same user shortens the same link.
+        #A preferable fix would be to update the associations to
+        #HABTM so that all users that shorten a long_url use
+        #the same short_url.
+        if current_user.links.where(long_url: url.long_url).exists?
+          redirect_to link_path(url.short_url)
+          return
+        else
+          @link = current_user.links.build(link_params)
+        end
       else
         redirect_to link_path(url.short_url)
         return
